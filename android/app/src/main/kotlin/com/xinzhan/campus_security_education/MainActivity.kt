23ba99e41ab2c3a31a44bc06274e354d094d5eee
ch4o5 <1150477170@qq.com>
2019-11-12 15:04:11 +0800

    init project

---
+++ android/app/src/main/kotlin/com/xinzhan/campus_security_education/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.xinzhan.campus_security_education
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
